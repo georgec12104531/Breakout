082e6ade77b1a0d9ae3877f35b9c2a843d811e48
George Chang <georgecharleschang@gmail.com>
2018-11-01 10:26:36 -0700

    Add event listeners to left and right paddle

--- breakout.js
+++ breakout.js
@@ -1,9 +1,83 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "teal";
-ctx.fill();
-ctx.closePath();
+var x = canvas.width/2;
+var y = canvas.height-30;
+//Ball number 2
+// var x1 = (canvas.width/2) + 2;
+// var y1 = canvas.height-30;
 
+var dx = 2;
+var dy = 2;
+var ballRadius = 5;
+
+
+//paddle
+var paddleHeight = 4;
+var paddleWidth = 60;
+var paddleX = (canvas.width-paddleWidth) / 2;
+
+//arrow keys
+var rightPressed = false;
+var leftPressed = false;
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  drawPaddle();
+
+  x += dx;
+  y += dy;
+
+  if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+
+  if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+
+  if (rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
+
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "teal";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "teal";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function keyDownHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = true;
+  } else if (e.keyCode == 37) {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = false;
+  } else if (e.keyCode == 37) {
+    leftPressed = false;
+  }
+}
+
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+setInterval(draw, 20);
